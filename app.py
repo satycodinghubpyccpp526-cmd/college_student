d76adf1d924818441045302fe184a899271943df
Satyam Shandilya <satycodinghubpyccpp526@gmail.com>
2026-05-19 20:51:26 +0530

    Add next  Line

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, session
 import sqlite3
 from datetime import datetime
 app = Flask(__name__)
+app.secret_key="college_secret"
 
 def init_db():
     conn = sqlite3.connect("college_intel.db")
@@ -15,23 +16,46 @@ def init_db():
               urgency TEXT,
               credibility INTEGER DEFAULT 0,
               created_at TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TEXT UNIQUE,
+              password TEXT)''')
     conn.commit()
     conn.close()
     
 init_db()
+
 @app.route("/")
 def home():
-
+    
+    college = request.args.get("college")
+    branch = request.args.get("branch")
+    urgency = request.args.get("urgency")
+    
     conn = sqlite3.connect("college_intel.db")
     c = conn.cursor()
+    query = "SELECT * FROM tips WHERE 1=1"
+    params = []
+    
+    if college:
+        query += " AND college=?"
+        params.append(college)
+        
+    if branch:
+        query += " AND branch=?"
+        params.append(branch)
+        
+    if urgency:
+        query += " AND urgency=?"
+        params.append(urgency)
 
-    c.execute("SELECT * FROM tips ORDER BY id DESC")
+    c.execute(query, params)
 
     tips = c.fetchall()
 
     conn.close()
 
     return render_template("index.html", tips=tips)
+
 @app.route("/add_tip", methods=["POST"])
 def add_tip():
 
@@ -63,6 +87,7 @@ def add_tip():
     conn.close()
 
     return redirect("/")
+
 @app.route("/verify/<int:id>")
 def verify(id):
     conn = sqlite3.connect("college_intel.db")
@@ -72,6 +97,48 @@ def verify(id):
     conn.close()
     
     return redirect("/")
+
+@app.route("/signup",methods = ["GET","POST"])
+def signup():
+    if request.method=="POST":
+        
+        username=request.form["username"]
+        password=request.form["password"]
+
+        conn=sqlite3.connect("college_itel.db")
+        c=conn.cursor()
+        c.execute("INSERT INTO users(username,password) VALUES(?,?)",(username,password))
+        conn.commit()
+        conn.close()
+        return redirect("/")
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        username = request.form["password"]
+        conn = sqlite3.connect("college_intel.db")
+        c = conn.cursor()
+        
+        c.execute(
+            "SELECT * FROM user WHERE username=? AND password=?",(username, password)
+        )
+        user = c.fetchone()
+        conn.close()
+        if user:
+            session["user"] = username
+            return redirect("/")
+        else:
+            return "Invalid Username or Password"
+        
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/")
+        
 @app.route("/tips", methods = ["GET"])
 def get_tips():
     conn = sqlite3.connect("college_intel.db")
